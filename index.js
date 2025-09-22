84a9faf27108b5ecd67132a61844aa360fb22864
diegoltursini <73371155+diegolimatavaresursini@users.noreply.github.com>
2025-09-22 20:56:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// Função para alternar a visibilidade do chatbot
-function toggleChatbot() {
-    const chatbotWindow = document.getElementById('chatbotWindow');
-    if (chatbotWindow.style.display === 'flex') {
-        chatbotWindow.style.display = 'none';
-    } else {
-        chatbotWindow.style.display = 'flex';
-    }
-}
+
